5ed7cba1d27022a2f90837aa92a6925dce121532
Ant Andrades <anthony.andrades@schneider-electric.com>
2022-03-31 08:20:10 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "ami" {}
+variable "region" {}
+variable "instance_type" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "subnet_id" {}
+
+resource "aws_instance" "terraform-cloud-demo" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id   = var.subnet_id
+  tags = {
+   Name = "terraform-cloud-demo-server" 
+  }
+}
