f08b408bb974f3a2337618d3a5c3f3070f3ceb12
Ant Andrades <anthony.andrades@schneider-electric.com>
2022-04-01 08:07:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "aws_instance" "terraform-cloud-demo" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id   = var.subnet_id
-  vpc_id = var.vpc_id
   tags = {
    Name = "terraform-cloud-demo-server" 
   }
